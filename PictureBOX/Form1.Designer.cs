93409e0f68306dbb696e17c41cad6fae87322832
Jose Luis <joseluismartinez1024@gmail.com>
2021-12-24 16:48:02 -0600

    Crear galería estática.

---
+++ PictureBOX/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace PictureBOX
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::PictureBOX.Properties.Resources._1;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(260, 237);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.pictureBox1);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(300, 300);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(300, 300);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Galeria";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ PictureBOX/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PictureBOX
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Se pone el intervalo del TIMER 
+            timer1.Interval = 300;
+            // Se activa el control
+            timer1.Enabled = true;
+            // Se inicia el temporizador
+            timer1.Start();
+        }
+        // Contador para ver las imagenes
+        int contador = 0;
+
+        // Evento de paso del temporizador
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // Cuando el contador lleve 100 se mostrara la imagen 1, cuando lleve 150 la 2
+            // y así sucesivamente....
+            if (contador == 100)
+                pictureBox1.Image = Properties.Resources._1;
+            if (contador == 150)
+                pictureBox1.Image = Properties.Resources._2;
+            if (contador == 200)
+                pictureBox1.Image = Properties.Resources._3;
+            if (contador == 250)
+                pictureBox1.Image = Properties.Resources._4;
+            // Se reinicia la primer imagen
+            if (contador == 300)
+            {
+                // Se muestra la primer imagen
+                pictureBox1.Image = Properties.Resources._1;
+                // Se vacia el contador para que vuelva a iniciar de 0 e ir incrementando
+                contador = 0;
+            }
+            // Incremento del contador para que tome valores entre cada intervalo del TIMER
+            contador += 50;
+        }
+    }
+}
